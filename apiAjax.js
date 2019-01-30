c31a9969db84dd9d2605ffe448a963448c5e54b1
Alaa Aqeel <alaa.21.iraq@gmail.com>
2019-01-30 15:11:23 -0800

    Update apiAjax.js

--- apiAjax.js
+++ apiAjax.js
@@ -4,6 +4,8 @@
  */
  
 (function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
 	(global.apiAjax = factory());
 }(this, ( function () { 
 
